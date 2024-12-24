51d487ad12a7ca50d40359a0f3e509d7ba13f3fd
Fjedor Gaede <fjedor.gaede@gmail.com>
2024-12-24 01:13:52 +0100

    Have tmux sessionizer and more

---
+++ .local/bin/tmux-sessionizer/tmux-reattach.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+switch_to() {
+    if [[ -z $TMUX ]]; then
+        tmux attach-session -t $1
+    else
+        tmux switch-client -t $1
+    fi
+}
+
+get_session_names() {
+    tmux ls | awk '{print $1}' | cut -d: -f1
+}
+
+selected_session=$(get_session_names | fzf)
+
+if [[ -z $selected_session ]]; then
+    exit 0
+fi
+
+switch_to $selected_session
