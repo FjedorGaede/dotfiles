9b4ba124b6c60c22b9c38c826d78769f977385ea
Fjedor Gaede <fjedor.gaede@gmail.com>
2025-01-02 18:45:45 +0100

    Add custom lsp information to the status line

--- .config/nvim/lua/config/plugins/mini/mini-statusline.lua
+++ .config/nvim/lua/config/plugins/mini/mini-statusline.lua
@@ -34,14 +34,21 @@ return {
         local git = " " .. (vim.b and vim.b.minigit_summary and vim.b.minigit_summary.head_name or "")
         local diff = MiniStatusline.section_diff({ trunc_width = 75 })
         local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-        local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
         local filename = MiniStatusline.section_filename({ trunc_width = 140 })
         -- local fileinfo_long = MiniStatusline.section_fileinfo({ trunc_width = 140 })
         local fileinfo = get_short_file_info()
-        local location = MiniStatusline.section_location({ trunc_width = 75 })
-        local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+        -- local location = MiniStatusline.section_location({ trunc_width = 75 })
+        -- local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
         local reg = vim.fn.reg_recording()
         local macro = "@" .. reg
+        -- local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+
+        local lsp_clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
+        local lsp_client_names = {}
+        for _, client in pairs(lsp_clients) do
+          table.insert(lsp_client_names, client.name)
+        end
+        local lsp_client_name = "(" .. table.concat(lsp_client_names, ", ") .. ")"
 
         local groups = {
           { hl = mode_hl, strings = { mode } },
@@ -50,7 +57,7 @@ return {
           "%<", -- Mark general truncate point
           { hl = "MiniStatuslineFilename", strings = { filename } },
           "%=", -- End left alignment
-          { hl = "MiniStatuslineFileinfo", strings = { fileinfo, lsp } },
+          { hl = "MiniStatuslineFileinfo", strings = { fileinfo, lsp_client_name } },
         }
 
         if reg ~= "" then
