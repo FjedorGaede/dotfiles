899489a6bf014ddf450c5ebfe3f033689e9c9428
Fjedor Gaede <fjedor.gaede@gmail.com>
2025-01-03 13:26:03 +0100

    Yank full path of file in mini.files

--- .config/nvim/lua/config/plugins/mini/mini-files.lua
+++ .config/nvim/lua/config/plugins/mini/mini-files.lua
@@ -17,6 +17,26 @@ return {
     end)
   end,
   init = function()
+    -- TAKEN FROM HELP
+
+    -- Yank in register full path of entry under cursor
+    local yank_path = function()
+      local path = (MiniFiles.get_fs_entry() or {}).path
+      if path == nil then
+        return vim.notify("Cursor is not on valid entry")
+      end
+      vim.fn.setreg(vim.v.register, path)
+      print("Yanked full path")
+    end
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "MiniFilesBufferCreate",
+      callback = function(args)
+        local b = args.data.buf_id
+        vim.keymap.set("n", "Y", yank_path, { buffer = b, desc = "Yank path" })
+      end,
+    })
+
     -- Add rounded border.
     vim.api.nvim_create_autocmd("User", {
       pattern = "MiniFilesWindowOpen",
