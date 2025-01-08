2f8c093d976793cc830d85f2cd08bbadee8cb8d3
Fjedor Gaede <fjedor@edyoucated.org>
2025-01-08 17:30:09 +0100

    Conform configure for typescript that it only runs one formatter at a time

--- .config/nvim/lua/config/plugins/conform.lua
+++ .config/nvim/lua/config/plugins/conform.lua
@@ -3,9 +3,10 @@ return {
     "stevearc/conform.nvim",
     config = function()
       require("conform").setup({
+        log_level = vim.log.levels.DEBUG,
         formatters_by_ft = {
           lua = { "stylua", lsp_format = "fallback" },
-          typescript = { "prettierd", "prettier", lsp_format = "fallback" },
+          typescript = { "biome", "prettierd", "prettier", lsp_format = "fallback", stop_after_first = true },
         },
         format_on_save = {
           timeout_ms = 500,
