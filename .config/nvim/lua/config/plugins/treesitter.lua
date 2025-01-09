dcee810601e371bfc9733b05d2400f6855d4ab6e
Fjedor Gaede <fjedor@edyoucated.org>
2025-01-09 16:15:33 +0100

    Do not disable tree sitter highlighting for the graphql files

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -17,6 +17,7 @@ return {
           "css",
           "scss",
           "html",
+          "graphql",
         },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
@@ -29,13 +30,11 @@ return {
         highlight = {
           enable = true,
 
-          -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-          -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-          -- the name of the parser)
-          -- list of language that will be disabled
-          disable = { "c", "rust" },
-          -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
           disable = function(lang, buf)
+            if lang == "graphql" then
+              return false -- We do not want to check here
+            end
+
             local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
