fd30a2948a0f87446835a8ba0b1e158be079e7db
Fjedor Gaede <fjedor.gaede@gmail.com>
2025-01-18 15:10:13 +0100

    disable notify because of error?

--- .config/nvim/lua/config/plugins/noice.lua
+++ .config/nvim/lua/config/plugins/noice.lua
@@ -1,31 +1,45 @@
 return {
-  "folke/noice.nvim",
-  enabled = true,
-  opts = {
-    cmdline = {
-      -- view = "cmdline",
+  {
+    "smjonas/inc-rename.nvim",
+    config = function()
+      require("inc_rename").setup({})
+    end,
+  },
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      "smjonas/inc-rename.nvim",
     },
-    lsp = {
-      signature = {
+    enabled = true,
+    opts = {
+      cmdline = {
+        -- view = "cmdline",
+      },
+      notify = {
         enabled = false,
       },
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+      lsp = {
+        signature = {
+          enabled = false,
+        },
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+        progress = {
+          enabled = false, -- We do this via snacks notifications
+        },
       },
-      progress = {
-        enabled = false, -- We do this via snacks notifications
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = true, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
       },
-    },
-    -- you can enable a preset for easier configuration
-    presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
-      command_palette = true, -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = true, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = true, -- add a border to hover docs and signature help
     },
   },
 }
