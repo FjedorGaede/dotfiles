331d0e2b3b194aa5552c136bb5dffda2997ab7ef
Fjedor Gaede <fjedor.gaede@gmail.com>
2025-01-21 22:55:21 +0100

    Focus the current file always in the neo-tree

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,20 +1,25 @@
+local set = vim.keymap.set
+
 -- Debugging
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
-vim.keymap.set("n", "<space>x", ":.lua<CR>", { silent = true })
-vim.keymap.set("v", "<space>x", ":lua<CR>", { silent = true })
+set("n", "<space><space>x", "<cmd>source %<CR>")
+set("n", "<space>x", ":.lua<CR>", { silent = true })
+set("v", "<space>x", ":lua<CR>", { silent = true })
 
 -- Remove search highlighting on esc key
-vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>", { silent = true })
+set("n", "<Esc>", ":nohlsearch<CR>", { silent = true })
 
 -- Run Tmux Sessionizer
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer.sh<CR>")
-vim.keymap.set("n", "<C-g>", "<cmd>silent !tmux neww tmux-reattach.sh<CR>")
+set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer.sh<CR>")
+set("n", "<C-g>", "<cmd>silent !tmux neww tmux-reattach.sh<CR>")
+
+-- Nvim Terminal
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 
 -- NOTE This feels kinda disruptive as it also let's me not use this when I acually want to use it in telescope or completion
--- vim.keymap.set("n", "<C-k>", "<C-w>k")
--- vim.keymap.set("n", "<C-j>", "<C-w>j")
--- vim.keymap.set("n", "<C-h>", "<C-w>h")
--- vim.keymap.set("n", "<C-l>", "<C-w>l")
+-- set("n", "<C-k>", "<C-w>k")
+-- set("n", "<C-j>", "<C-w>j")
+-- set("n", "<C-h>", "<C-w>h")
+-- set("n", "<C-l>", "<C-w>l")
 --
 
 -- REMOVE DEFAULT MAPPINGS --
@@ -22,7 +27,7 @@ vim.keymap.set("n", "<C-g>", "<cmd>silent !tmux neww tmux-reattach.sh<CR>")
 -- Remove the keymaps as I want to use g differently as a `go to` keymap
 local function removeKeymapIfExists(mode, keymap)
   local _, _ = pcall(function()
-    vim.keymap.del(mode, keymap)
+    del(mode, keymap)
   end)
 end
 
@@ -33,5 +38,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
     removeKeymapIfExists("n", "grr")
     removeKeymapIfExists("n", "gra")
     removeKeymapIfExists("n", "grn")
+
+    -- vim.keymap.set("n", "<leader>O", require("lspconfig").vtsls.organize_imports)
+    vim.keymap.set("n", "<leader>O", function()
+      require("lspconfig").vtsls.organize_imports()
+    end)
   end,
 })
